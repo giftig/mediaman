547d7c2c1a55dd0f9b0d1e7a5ffeb2f20ea56228
Rob Moore <giftiger.wunsch@xantoria.com>
2014-12-29 20:55:28 +0000

    Add build script
    Also change the package name to mediaman; it's better.

---
+++ scripts/build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [[ "$1" == "" ]]; then
+  echo 'You need to specify a production config file to build into the package'
+  exit 1
+fi
+CONFIG_FILE="$1"
+
+if [[ ! $(ls "$CONFIG_FILE") ]]; then
+  echo 'The config file does not exist. Check the path?'
+  exit 1
+fi
+
+cp "$CONFIG_FILE" src/main/resources/application.conf
+
+mvn test
+
+if [[ "$?" != 0 ]];
+  echo 'Tests failed; aborting build.'
+  exit 2
+fi
+
+mvn clean package
+
+if [[ "$?" != 0 ]];
+  echo 'Package step failed!'
+  exit 3
+fi
+
+echo 'Looks like everything went fine.'
+echo "The following files were built:"
+
+ls build/mediaman*.jar
+
+# Don't need this anymore
+rm src/main/resources/production.conf
